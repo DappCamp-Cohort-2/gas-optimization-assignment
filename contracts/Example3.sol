c0356302bb6d6d8b9824721b16b9c40b7bf5ac15
Prashant Nigam <ajaxbuddy@gmail.com>
2022-01-26 18:20:06 -0500

    Assignment submission
    Assignment submission

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -2,13 +2,11 @@ pragma solidity ^0.8.4;
 
 contract Example3 {
     uint256 marginPercentage = 30;
-    function getOwnerMargin(uint256 amount)
-        public
-        view
-        returns (uint256 amountForSender, uint256 amountForOwner)
-    {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+
+    function getOwnerMargin(uint256 amount) public view returns (uint256 amountForSender, uint256 amountForOwner) {
+        uint _margingPercentage = marginPercentage;
+        amountForSender = (amount * (100 - _margingPercentage)) / 100;
+        amountForOwner = (amount * _margingPercentage) / 100;
     }
 
 }
