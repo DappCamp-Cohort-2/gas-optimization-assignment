8280c23ca549e2a7022bcecfa6c80c5df581e6e2
Prashant Nigam <ajaxbuddy@gmail.com>
2022-01-26 18:23:30 -0500

    Assignment submission - further reduced gas in example3
    Assignment submission - further reduced gas in example3

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.4;
 
 contract Example3 {
-    uint256 marginPercentage = 30;
+    // uint256 marginPercentage = 30;
 
-    function getOwnerMargin(uint256 amount) public view returns (uint256 amountForSender, uint256 amountForOwner) {
-        uint _margingPercentage = marginPercentage;
+    function getOwnerMargin(uint256 amount) public pure returns (uint256 amountForSender, uint256 amountForOwner) {
+        uint _margingPercentage = 30;
         amountForSender = (amount * (100 - _margingPercentage)) / 100;
         amountForOwner = (amount * _margingPercentage) / 100;
     }
