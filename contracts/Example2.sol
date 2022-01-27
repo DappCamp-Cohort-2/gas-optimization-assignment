b5d975331120fc6751156733e852a005dd9b9208
Luis Naranjo <luisnaranjo733@gmail.com>
2022-01-26 22:13:32 -0800

    example 2

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
+        uint _counter; // increment a memory counter locally and then update storage once at the end
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            _counter += arr[idx];
         }
+        counter = _counter;
     }
 }
