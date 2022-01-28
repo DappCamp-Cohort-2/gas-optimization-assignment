bddc52265c9d04975c4f72786fe451014531a09a
charlie-kim <charlie@qanda.la>
2022-01-27 22:24:30 -0800

    optimize contracts

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
+        uint arrSum;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            arrSum += arr[idx];
         }
+        counter += arrSum;
     }
 }
