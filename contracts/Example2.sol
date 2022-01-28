7b3dbf37841cee81d71da528c634fb64a9b6d731
Raghuramkowdeed <raghuramkowdeed@gmail.com>
2022-01-27 21:40:49 -0800

    Update Example2.sol

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
         for (uint idx = 0; idx < arr.length; idx++){
             counter += arr[idx];
         }
