9be6bd564bf1f0c17a13ba4f199e13c613de527e
Warrenn <warrenn@busyweb.co.za>
2022-01-29 16:16:09 +0200

    Warrenn Enslin

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.8.4;
 
 contract Example2 {
-    uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    uint8 public counter;
+
+    function incrementBy(uint8[] calldata arr) external {
+        uint8 _local;
+        for (uint8 idx = 0; idx < arr.length; idx++) {
+            _local += arr[idx];
         }
+        counter = _local;
     }
 }
