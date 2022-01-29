2104e3652fcf3066c29ddc740b0d98a966d03f14
Maria V <maria.varvaroi@thermofisher.com>
2022-01-29 08:48:33 +0200

    reduce gas costs

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,16 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+// https://mudit.blog/solidity-gas-optimization-tips/ - #10
+
 contract Example2 {
-    uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    uint256 public counter;
+
+    function incrementBy(uint256[] calldata arr) external {
+        uint256 j;
+        for (uint256 idx = 0; idx < arr.length; idx++) {
+            j += arr[idx];
         }
+        counter = j;
     }
 }
