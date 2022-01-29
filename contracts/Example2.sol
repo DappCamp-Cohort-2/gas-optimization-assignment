3068ce29383f6d3a80bb4dcdf8a3de9ec5f629d7
Charles Naut <cpn585@gmail.com>
2022-01-29 11:22:14 -0500

    Optimize gas for example 2 by using assembly

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -4,8 +4,13 @@ contract Example2 {
     uint public counter;
  
     function incrementBy(uint[] memory arr) public {
+        uint total = counter;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            assembly {
+                total := add(total, mload(add(add(arr, 0x20), mul(idx, 0x20))))
+            }
         }
+
+        counter = total;
     }
 }
