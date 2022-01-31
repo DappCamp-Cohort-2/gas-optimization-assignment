903d6a7d1a1369e58d0c5385bc2d46720836e745
Tha Franklin <thalita.franklyn@gmail.com>
2022-01-31 18:18:42 -0300

    optimizing gas

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,14 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
+        uint sum_arr;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            sum_arr += arr[idx];
         }
+        counter += sum_arr;
     }
 }
