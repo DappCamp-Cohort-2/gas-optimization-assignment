147ed900445796e5e9e8abf69702c7f35e581627
Brian Murphy <briancmurphy87@uchicago.edu>
2022-01-31 16:06:29 -0600

    assignment submission

--- contracts/Example1.sol
+++ contracts/Example1.sol
@@ -1,7 +1,27 @@
 pragma solidity ^0.8.4;
 
 contract Example1 {
-    uint8 public day = 10;
-    uint256 public amount = 1000;
-    address public ownerAddr = 0x4e653b730367e303D30d8FB39d7350D07a60dfD6;
+
+    /*
+    original layout:
+    arg1: day -> size: 008 bits
+    arg2: amt -> size: 256 bits
+    arg3: adr -> size: 160 bits (20 bytes)
+    */
+
+    // optimization 1a: packing --> put day and address next to each other (amount gets its own mem block)
+    // optimization 1b: address + day = 160 + 8 = 168 bits
+    // -> leaves 256 - 168 = 88 bits on the block
+    // --> can use up to a uint64 for amount
+    uint8 public day;
+    uint64 public amount;
+    address public ownerAddr;
+
+    // optimization 2: set values in ctor
+    constructor() {
+        day = 10;
+        amount = 1000;
+        ownerAddr = 0x4e653b730367e303D30d8FB39d7350D07a60dfD6;
+    }
+
 }
--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -2,10 +2,15 @@ pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
+
+    // optimization: 'calldata' instead of 'memory' (not altering input arr arg)
+    function incrementBy(uint[] calldata arr) public {
+        // optimization: less reads on 'counter' -> maintain function-local sum of arr
+        uint arrSum;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            arrSum += arr[idx];
         }
+        // optimization: less reads on 'counter'
+        counter += arrSum;
     }
 }
