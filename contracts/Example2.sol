4c8e11f4a803a6710a67449146001275fe78e5d7
Zach Frey <zach@znfrey.com>
2022-02-04 21:47:24 -0500

    Some gas reduction, not enough yet

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,16 @@
 pragma solidity ^0.8.4;
 
+// Adding with sum reduces gas from 50355 to 49467
+// Not enough yet!
+
 contract Example2 {
     uint public counter;
  
     function incrementBy(uint[] memory arr) public {
+        uint sum  = 0;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            sum += arr[idx];
         }
+        counter += sum;
     }
 }
