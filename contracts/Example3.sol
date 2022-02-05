095b1c2bf714092e0bf6c1e7bd13dc80238fad6a
Zach Frey <zach@znfrey.com>
2022-02-04 21:43:18 -0500

    Passes test by replacing unmodifiable variable with constant

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.8.4;
 
+// uint256 marginPercentage not needed
+// Not public, no getter/setter, it's just a constant.
+// Replace with '30'
+
 contract Example3 {
-    uint256 marginPercentage = 30;
+    //uint256 marginPercentage = 30;
     function getOwnerMargin(uint256 amount)
         public
         view
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        amountForSender = (amount * (100 - 30)) / 100;
+        amountForOwner = (amount * 30) / 100;
     }
 
 }
