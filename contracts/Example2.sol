ef00d57591e7c61e8a366e0a338724b39486bd54
Róbert Istók <robertistok94@gmail.com>
2022-02-08 14:13:54 +0000

    Update Example2.sol

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint256 public counter;
 
-    function incrementBy(uint256[] calldata arr) public {
+    function incrementBy(uint256[] calldata arr) external {
         uint256 _counter = counter;
         for (uint256 idx = 0; idx < arr.length; idx++) {
             _counter += arr[idx];
