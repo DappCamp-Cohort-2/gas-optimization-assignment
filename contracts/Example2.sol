3cb31a221f5bff3ba965b9c7bd467b2ab002dcd2
Zach Frey <zach@znfrey.com>
2022-02-10 11:06:31 -0500

    Change from public to external

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.8.4;
 
-// Adding with sum reduces gas from 50355 to 49467
-// Not enough yet!
-
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] calldata arr) public {
+    function incrementBy(uint[] calldata arr) external {
         uint sum = 0;
         for (uint idx = 0; idx < arr.length; idx++){
             sum += arr[idx];
