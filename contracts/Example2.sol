f7972b00e658694f3ba2b6b7eadea34760a71660
Mayuko Kondo <mayuko.kondo@outlook.com>
2022-02-10 13:58:02 +0300

    updated according to comments.

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] calldata arr) public {
-        uint sum = 0;
+    function incrementBy(uint[] calldata arr) external {
+        uint sum;
         for (uint idx = 0; idx < arr.length; idx++){
             sum += arr[idx];
         }
